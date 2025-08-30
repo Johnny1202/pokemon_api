548b645f01d558b3c71a86233294d7d8ca7b1002
jonathanmorales442 <jonathanmorales442@gmail.com>
2025-08-29 21:46:03 -0400

    design

--- src/components/PokemonList/SearchFilter.jsx
+++ src/components/PokemonList/SearchFilter.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-export default function SearchFilter({ searchTerm, onSearchChange, showFavoritesOnly, onToggleFavorites }) {
+export default function SearchFilter({
+  searchTerm,
+  onSearchChange,
+  showFavoritesOnly,
+  onToggleFavorites,
+}) {
   return (
     <div className="flex justify-between items-center mb-4 gap-4">
       <input
@@ -10,21 +15,17 @@ export default function SearchFilter({ searchTerm, onSearchChange, showFavorites
         onChange={(e) => onSearchChange(e.target.value)}
         className="flex-1 p-2 border rounded-md"
       />
-        {/* Checkbox de favoritos */}
-      <label className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-50 transition-colors duration-200 cursor-pointer">
-        <div className="relative inline-flex items-center cursor-pointer">
-          <input
-            type="checkbox"
-            checked={showFavoritesOnly}
-            onChange={(e) => onToggleFavorites(e.target.checked)}
-            className="sr-only"
-          />
-          <div className={`w-11 h-6 rounded-full transition-colors duration-200 ${showFavoritesOnly ? 'bg-indigo-600' : 'bg-gray-300'}`}>
-            <div className={`absolute top-0.5 left-0.5 bg-white border border-gray-300 rounded-full h-5 w-5 transition-transform duration-200 transform ${showFavoritesOnly ? 'translate-x-5' : ''}`}></div>
-          </div>
-        </div>
-        <span className="text-sm font-medium text-gray-700">Show Favorites Only</span>
-      </label>
+      <div className="flex flex-wrap items-center gap-2">
+        <input
+          type="checkbox"
+          id="favorites"
+          checked={showFavoritesOnly}
+          onChange={(e) => onToggleFavorites(e.target.checked)}
+        />
+        <label htmlFor="favorites" className="text-sm">
+          Show Favorites Only
+        </label>
+      </div>
     </div>
   );
 }
